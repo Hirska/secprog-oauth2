3d213507ac2f8e37b97d6f5caefe43f6a3af3ae7
Hirska <aleksij.hirvonen@pp.inet.fi>
2021-05-11 15:19:02 +0300

    Add dynamic client registration

---
+++ src/controllers/registerClient.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express';
+import { ObjectId } from 'mongoose';
+import { ZodError } from 'zod';
+import Client from '../models/client';
+import { newClientSchema } from '../utils/parse';
+import { createSHA256Hash, randomBytesAsync } from '../utils/utils';
+
+export const registerClient = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const { clientName, isConfidential, redirectUris } = newClientSchema.parse(req.body);
+    const clientSecret = isConfidential ? createSHA256Hash(await randomBytesAsync(64)) : undefined;
+    const newClient = new Client({
+      clientName,
+      isConfidential,
+      redirectUris,
+      clientSecret,
+      user: req.user?._id as ObjectId
+    });
+
+    const client = await newClient.save();
+    res.json({
+      clientId: client.clientId,
+      clientName: client.clientName,
+      clientSecret,
+      redirectUris: client.redirectUris
+    });
+    return;
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(400).json({ error: 'Invalid request: clientName, isConfidential, redirectUris required' });
+      return;
+    }
+    if (error.code === 11000) {
+      res.status(400).json({ error: 'Client with that clientName already exists' });
+      return;
+    }
+    return next(error);
+  }
+};
