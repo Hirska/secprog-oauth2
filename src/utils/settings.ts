9f8f8d91f4be9f8739d92e555e5a7a1d7999dc0c
Hirska <aleksij.hirvonen@pp.inet.fi>
2021-05-11 15:24:35 +0300

    Remove unnecessary env-variables

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -8,9 +8,6 @@ export default {
   ADMIN_EMAIL: env.get('ADMIN_EMAIL').required().asString(),
   ADMIN_PASSWORD: env.get('ADMIN_PASSWORD').required().asString(),
 
-  CLIENT_SECRET: env.get('CLIENT_SECRET').required().asString(),
-  CLIENT_REDIRECT_URL: env.get('CLIENT_REDIRECT_URL').required().asString(),
-
   JWT_SECRET: env.get('JWT_SECRET').required().asString(),
   JWT_LIFETIME: env.get('JWT_LIFETIME').required().asInt(),
 
