cb3d97d09bec9b1241ce9a092008bd004deb2749
Hirska <aleksij.hirvonen@pp.inet.fi>
2021-05-13 16:15:27 +0300

    feat: add openapi-specification

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,13 @@ import exphbs from 'express-handlebars';
 import handleError from './utils/errorHandler';
 import controllers from './controllers';
 import path from 'path';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import { connectToDB } from './db';
 
 const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const swaggerDocument = YAML.load(`${__dirname}/../openapi.yaml`);
 
 connectToDB();
 
@@ -29,6 +33,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 
 app.use(morgan('tiny'));
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', controllers);
 app.use((_req, res) => {
   res.sendStatus(404);
